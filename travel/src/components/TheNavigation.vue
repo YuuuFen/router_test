e149b4d2548d8a930e828e96556bdf8c5fb23629
YuFen <yufenhuangtaiwan@gmail.com>
2022-06-20 13:22:29 +0800

    clean up and DRY(Don’t Repeat Yourself)

--- travel/src/components/TheNavigation.vue
+++ travel/src/components/TheNavigation.vue
@@ -5,7 +5,10 @@
     <router-link
       v-for="destination in destinations"
       :key="destination.id"
-      :to="{ name: 'destination.show', params: { id: destination.id } }"
+      :to="{
+        name: 'destination.show',
+        params: { id: destination.id, slug: destination.slug },
+      }"
     >
       {{ destination.name }}
     </router-link>
