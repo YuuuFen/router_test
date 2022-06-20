d3fcc4cc5e7994a14b95b91fdad9fea4345a5052
YuFen <yufenhuangtaiwan@gmail.com>
2022-06-20 23:03:36 +0800

    go back

--- travel/src/views/DestinationShow.vue
+++ travel/src/views/DestinationShow.vue
@@ -13,27 +13,15 @@
 <script>
 import sourceData from "@/data.json";
 export default {
-  data() {
-    return {
-      destination: null,
-    };
-  },
   computed: {
     destinationId() {
       return parseInt(this.$route.params.id);
     },
-  },
-  methods: {
-    async initData() {
-      const response = await fetch(
-        `https://travel-dummy-api.netlify.app/${this.$route.params.slug}`
+    destination() {
+      return sourceData.destinations.find(
+        (destination) => destination.id == this.destinationId
       );
-      this.destination = await response.json();
     },
-  },
-  async created() {
-    this.initData();
-    // this.$watch(() => this.$route.params, this.initData);
   },
 };
 </script>
